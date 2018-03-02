bbb73253695d9a3708eb009204956c57840cb8fb
Adjectival <alexd.jacks@gmail.com>
2018-03-01 17:19:37 -0800

    :sparkles: 1st use of property binding with event listeners

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,27 +5,49 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <h1 class="app">
-      <img [src]="logo" style="max-width:10vw;">
-      {{ title }}
-      <img [src]="logo" style="max-width:10vw;">
+      <img [src]="logo">
+      {{ name +' '+ verb +' '+ object }}
+      <img [src]="logo">
     </h1>  
       
-    <input type="text" [value]="name">
-    <div>{{ name }}
-    </div>
+    <h2>One-way data binding aka Property Binding with Event (Blur) Listener</h2>
+    <ul>
+    <input type="text" [value]="name"
+      (blur)="handleBlur1($event)">
+    <input type="text" [value]="verb"
+      (blur)="handleBlur2($event)">
+    <input type="text" [value]="object"
+      (blur)="handleBlur3($event)">
+    </ul>
+
     <h3>Ternary expression w/ Unicode Happiness?
       {{ isHappy ? '😂' : '🤨' }}
-      <button>Happiness</button>
+      <button>Change Happiness</button>
     </h3>
-    
   `
 })
 export class AppComponent {
   title: string;
   logo: string = 'img/radioactive.svg';
   name: string = 'Jacks';
+  verb: string = 'Kicks';
+  object: string = 'Ass';
+
+  handleBlur1(event: any) {
+    this.name = event.target.value;
+    console.log(event);
+  }
+  handleBlur2(event: any) {
+    this.verb = event.target.value;
+    console.log(event);
+  }
+  handleBlur3(event: any) {
+    this.object = event.target.value;
+    console.log(event);
+  }
+
   isHappy: boolean = false;
   constructor() {
-    this.title = 'Jacks Kicks Ass';
+    this.verb = 'Kicks';
   }
 }
